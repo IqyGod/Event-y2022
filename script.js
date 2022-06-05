adc92b6ba8c2378431ca75e5ec74ce4872e551f4
IqyGod <99662300+IqyGod@users.noreply.github.com>
2022-06-05 13:20:51 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("demo").innerHTML = Date();
-
-var then =  new Date(2022,02,02),
-    today = new Date;
-
-document.getElementById("wait").innerHTML = Math.round((today - then) / (1000 * 60 * 60 * 24))
